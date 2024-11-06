87e5176f70e84f3918acfcc767f7b8c017841c74
selman-s <gunduzgece546@gmail.com>
2024-11-06 16:16:35 +0300

    Improve console log format for API_URL in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_BASE_URL+'/api';
-console.log(API_URL);
+console.log("API_URL : ",API_URL);
 
 const api = axios.create({
   baseURL: API_URL,
